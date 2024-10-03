bf02ef28a735e19ce0098554cf26ca5751fadad1
Eunomia <eunomia.dice@gmail.com>
2024-10-03 14:44:49 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// Ajout d'un scroll fluide lors du clic sur les liens de navigation
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        window.scrollTo({
+            top: targetSection.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
